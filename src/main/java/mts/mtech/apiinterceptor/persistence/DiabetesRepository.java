6dd31ccb9deb01b386cb6251235beed6a1eaee90
Mitch <tawandamitch@gmail.com>
2025-10-20 21:41:14 +0200

    removed diabetes code base from main branch

--- src/main/java/mts/mtech/apiinterceptor/persistence/DiabetesRepository.java
+++
@@ -1,9 +0,0 @@
-package mts.mtech.apiinterceptor.persistence;
-
-import mts.mtech.apiinterceptor.domain.Diabetes;
-
-import java.util.Optional;
-
-public interface DiabetesRepository extends BaseRepository<Diabetes>{
-    Optional<Diabetes> findByReferenceId(String referenceId);
-}
